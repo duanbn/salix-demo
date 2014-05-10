84f23542a9f1c826ebcf27c98d399faf964c5996
Asia <Asia@192.168.6.16>
2014-05-10 13:41:20 +0800

    从pinus-demo迁移到salix-demo

---
+++ src/test/java/com/salix/demo/MainTest.java
@@ -0,0 +1,38 @@
+package com.salix.demo;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.pinus.demo.IEchoService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+public class MainTest {
+
+	@Resource(name = "echo")
+	private IEchoService echoService;
+
+	@Test
+	public void test() {
+		String value = echoService.echo("hello pinus");
+		System.out.println(value);
+		echoService.discard();
+		echoService.discard("discard pinus");
+
+		try {
+			echoService.error();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			echoService.rterror();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
