a98d4f383fb709b047857e45a65cc46adcf20c50
Asia <Asia@192.168.6.16>
2014-05-14 18:46:10 +0800

    添加shutdown

--- src/test/java/com/salix/demo/MainTest.java
+++ src/test/java/com/salix/demo/MainTest.java
@@ -10,12 +10,20 @@ import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.salix.server.Shutdown;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:applicationContext.xml" })
 public class MainTest {
 
-	@Resource(name = "echo")
+	@Resource
 	private IEchoService echoService;
+
+	@Test
+	public void testShutdown() throws Exception {
+		Shutdown sd = new Shutdown("localhost", 9999);
+		sd.doShutdown();
+	}
 
 	@Test
 	public void test() throws Exception {
@@ -44,7 +52,7 @@ public class MainTest {
 		}
 
 		public void run() {
-			for (int i = 0; i < 100; i++) {
+			for (int i = 0; i < 1; i++) {
 				System.out.println(this.echoService.echo("salix concurrent " + i));
 			}
 		}
